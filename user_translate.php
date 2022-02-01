775a812ff5c9f0ca81326f3f678d47f8025c4102
Noah Groß <me@codesalat.dev>
2022-02-01 21:55:34 +0100

    Make sure we compare the same data

--- user_translate.php
+++ user_translate.php
@@ -1,7 +1,7 @@
 <?php
 function user_translate($original) {
     $user_translation = json_decode(getenv("USER_TRANSLATION"), true);
-    if(!array_key_exists($original, $user_translation)) {
+    if(!array_key_exists(strtolower($original), $user_translation)) {
         return $original;
     }
     return $user_translation[$original];
